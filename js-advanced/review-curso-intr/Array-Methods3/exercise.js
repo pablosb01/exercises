717cdd0afb42b241d15fb88cf1ddbde78f2e1904
pablosb01 <pablosb01@icloud.com>
2024-02-23 13:00:43 +0100

    Array3

---
+++ js-advanced/review-curso-intr/Array-Methods3/exercise.js
@@ -0,0 +1,25 @@
+function calculateAverageAge(persons) {
+  let sumaEdades = 0;
+  for(let i = 0; i < persons.length; i++){
+    sumaEdades += persons[i].age;
+  }
+  let edadMedia = sumaEdades / persons.length;
+  return edadMedia;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
