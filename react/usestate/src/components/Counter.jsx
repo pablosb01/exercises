7f9aa30274494d6b677c653bb8af930ed1bc2940
pablosb01 <pablosb01@icloud.com>
2024-04-19 18:42:07 +0200

    useState finished

---
+++ react/usestate/src/components/Counter.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import CounterDisplay from "./CounterDisplay";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    function addOne() {
+        setCount(count + 1)
+    }
+
+    function reset() {
+        setCount(0)
+    }
+
+    return(
+        <>
+            <CounterDisplay count={count} />
+            <button onClick={addOne}>add one</button>
+            <button onClick={reset}>reset</button>
+        </>
+    )
+}
+
+export default Counter
