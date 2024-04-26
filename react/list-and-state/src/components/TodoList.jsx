3fb659d85379b38e0afbaea193cf7c4f6f58a3dd
pablosb01 <pablosb01@icloud.com>
2024-04-26 18:44:00 +0200

    list and state exercise

--- react/list-and-state/src/components/TodoList.jsx
+++ react/list-and-state/src/components/TodoList.jsx
@@ -19,15 +19,30 @@ function TodoList() {
         }
     }
 
+    function resetTodo() {
+        setTodos([])
+    }
+
+    function removeTodo (index) {
+        const updatedTodos = [...todos];
+        updatedTodos.splice(index, 1);
+        setTodos(updatedTodos)
+    }
+
+
     return(
         <>
             <ul>
                 {todos.map((todo, index) => (
-                    <li key={index}>{todo}</li>
+                    <li key={index}>
+                        {todo}
+                        <button onClick={() => removeTodo(index)}>Remove</button>
+                    </li>
                 ))}
             </ul>
             <input type='text' value={newTodo} onChange={(e) => setNewTodo(e.target.value)}></input>
             <button onClick={addTodo}>Add</button>
+            <button onClick={resetTodo}>Reset List</button>
         </>
     )
 } 
