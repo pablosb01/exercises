479c027b00968311ffd237c8132c4708520eb4b8
pablosb01 <pablosb01@icloud.com>
2024-06-04 15:27:20 +0200

    exercise promises lucky draw

---
+++ nodejs/PromisesLuckyDraw/LuckyDraw.js
@@ -0,0 +1,21 @@
+const players = ['Joe', 'Caroline', 'Sabrina'];
+
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+}
+
+players.forEach(player => {
+    luckyDraw(player)
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+})
